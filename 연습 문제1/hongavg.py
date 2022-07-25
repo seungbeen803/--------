48e9d602f97639d4e522f851a0df8af6eec796e2
seungbeen <seungbin803@gmail.com>
2022-07-25 21:45:28 +0900

    7월 25일 파이썬 공부-연습문제 풀기

--- 연습 문제1/hongavg.py
+++ 연습 문제1/hongavg.py
@@ -1,1 +1,9 @@
 # 1. 홍길동 씨의 과목별 점수는 다음과 같다. 홍길동 씨의 평균 점수를 구해 보자.
+korea = 80
+english = 75
+math = 55
+sum = korea + english + math
+avg = sum/3
+# avg = korea + english + math/3
+
+print("홍길동 씨의 평균 점수 : ", avg)
---
+++ 연습 문제1/replace.py
@@ -0,0 +1,5 @@
+# 5. 다음과ㅏ 같은 문자열 a:b:c:d가 있다. 
+# 문자열의 replace 함수를 사용하여 a#b#c#d로 바꿔서 출력해 보자.
+a = "a:b:c:d"
+b = a.replace(":","#")
+print(b)
